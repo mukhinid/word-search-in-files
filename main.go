30a7559aa4049ee97fa43aa8ae528dabb17593dc
Mukhin Igor <mukhinid@yandex.ru>
2024-03-12 18:18:17 +0300

    Реализовал поиск через эндпоинт.

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"net/http"
+	"os"
+
+	"word-search-in-files/pkg/searcher"
 
 	"github.com/gin-gonic/gin"
 )
 
+var searchModule searcher.Searcher
+
 func searchInFiles(c *gin.Context) {
 	word := c.Param("word")
 
-	c.IndentedJSON(http.StatusOK, word)
+	files, _ := searchModule.Search(word)
+
+	c.IndentedJSON(http.StatusOK, files)
 }
 
 func main() {
+	searchModule = searcher.Searcher{
+		FS: os.DirFS("examples"),
+	}
+
 	router := gin.Default()
 	router.GET("/files/search/:word", searchInFiles)
 
