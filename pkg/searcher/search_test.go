785c40b892f2511a6b37996c8cb7f5201a76f63d
Mukhin Igor <mukhinid@yandex.ru>
2024-03-13 23:38:52 +0300

    Добавил тесты.

--- pkg/searcher/search_test.go
+++ pkg/searcher/search_test.go
@@ -1,11 +1,18 @@
 package searcher
 
 import (
+	"errors"
 	"io/fs"
 	"reflect"
 	"testing"
 	"testing/fstest"
 )
+
+type ErrorFS struct{}
+
+func (f ErrorFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("test error occured")
+}
 
 func TestSearcher_Search(t *testing.T) {
 	type fields struct {
@@ -33,6 +40,28 @@ func TestSearcher_Search(t *testing.T) {
 			args:      args{word: "World"},
 			wantFiles: []string{"file1", "file3"},
 			wantErr:   false,
+		},
+		{
+			name: "Word not found",
+			fields: fields{
+				FS: fstest.MapFS{
+					"file1.txt": {Data: []byte("World")},
+					"file2.txt": {Data: []byte("World1")},
+					"file3.txt": {Data: []byte("Hello World")},
+				},
+			},
+			args:      args{word: "World2"},
+			wantFiles: nil,
+			wantErr:   false,
+		},
+		{
+			name: "Error on file process",
+			fields: fields{
+				FS: ErrorFS{},
+			},
+			args:      args{word: "World"},
+			wantFiles: nil,
+			wantErr:   true,
 		},
 	}
 	for _, tt := range tests {
