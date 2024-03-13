5cc875e029fddbce4db21dd72276282ca6fa056c
Mukhin Igor <mukhinid@yandex.ru>
2024-03-13 13:31:43 +0300

    Реализовал синхронный поиск через индекс.

--- pkg/searcher/search.go
+++ pkg/searcher/search.go
@@ -1,68 +1,63 @@
 package searcher
 
 import (
+	"errors"
 	"io/fs"
 	"path/filepath"
+	"slices"
 	"strings"
-	"sync"
 	"word-search-in-files/pkg/internal/dir"
 )
 
 // Searcher позволяет искать ключевое слово в файлах указанной папки.
 type Searcher struct {
-	FS fs.FS // Файловая система, предоставляющая папку, в файлах которой осуществляется поиск.
+	FS           fs.FS               // Файловая система, предоставляющая папку, в файлах которой осуществляется поиск.
+	wordFilesMap map[string][]string // Словарь соответствия слов и файлов.
+}
+
+// Инициализирует словарь для поиска.
+func (s *Searcher) Init() error {
+	s.wordFilesMap = make(map[string][]string)
+
+	files, err := dir.FilesFS(s.FS, ".")
+
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		fileWihtoutExt := getFileNameWithoutExtension(file)
+		content, fileErr := fs.ReadFile(s.FS, file)
+
+		if fileErr != nil {
+			continue
+		}
+
+		words := strings.Fields(string(content))
+		for _, word := range words {
+			entry, wordContains := s.wordFilesMap[word]
+			if wordContains {
+				if !slices.Contains(entry, fileWihtoutExt) {
+					s.wordFilesMap[word] = append(entry, fileWihtoutExt)
+				}
+			} else {
+				s.wordFilesMap[word] = []string{fileWihtoutExt}
+			}
+		}
+	}
+
+	return nil
 }
 
 // Производит поиск слова word по файлам в папке файловой системы.
 func (s *Searcher) Search(word string) ([]string, error) {
-	files, err := dir.FilesFS(s.FS, ".")
-
-	if err != nil {
-		return files, err
+	if s.wordFilesMap == nil {
+		return nil, errors.New("непроинциализирован словарь поиска, вызовите функцию Searcher.Init(), прежде чем искать")
 	}
 
-	result := make([]string, 0, len(files))
+	files := s.wordFilesMap[word]
 
-	var wg sync.WaitGroup
-	ch := make(chan string)
-
-	for _, file := range files {
-		wg.Add(1)
-
-		go func(file string) {
-			searchInFile(s.FS, file, word, ch)
-			wg.Done()
-		}(file)
-	}
-
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
-
-	for file := range ch {
-		result = append(result, getFileNameWithoutExtension(file))
-	}
-
-	return result, err
-}
-
-// Производит поиск слова word по файлу filename в файловой системе filesystem. Если слово содержится в файле - отправляет filename в канал ch.
-func searchInFile(filesystem fs.FS, filename string, word string, ch chan string) {
-	content, err := fs.ReadFile(filesystem, filename)
-
-	if err != nil {
-		return
-	}
-
-	words := strings.Fields(string(content))
-
-	for _, w := range words {
-		if w == word {
-			ch <- filename
-			return
-		}
-	}
+	return files, nil
 }
 
 // Возвращает имя файла filename без расширения.
